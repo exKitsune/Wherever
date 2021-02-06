2bc9ae3b3ba423d2c59cfcf72e83d051d073389d
fruit <danieltapham@gmail.com>
2021-02-05 23:38:13 -0500

    Implemented quick settings Implemented no connection auto off

---
+++ Phone/Wherever/app/src/main/java/com/fruit/wherever/ModifyRecord.java
@@ -0,0 +1,52 @@
+package com.fruit.wherever;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ModifyRecord extends Activity implements View.OnClickListener {
+    DBManager dbManager = new DBManager(this);
+    TextView modTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Delete Record?");
+
+        setContentView(R.layout.activity_modify_record);
+
+        Button deleteBtn = (Button) findViewById(R.id.delete_btn);
+        Button cancelBtn = (Button) findViewById(R.id.cancel_btn);
+        modTextView = (TextView) findViewById(R.id.modTextView);
+        Intent intent = getIntent();
+        String host = intent.getStringExtra("host");
+        modTextView.setText(host);
+
+        deleteBtn.setOnClickListener(this);
+        cancelBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.cancel_btn:
+                this.returnHome();
+                break;
+            case R.id.delete_btn:
+                String host = modTextView.getText().toString();
+                dbManager.open();
+                dbManager.delete(host);
+                dbManager.close();
+                this.returnHome();
+                break;
+        }
+    }
+
+    public void returnHome() {
+        Intent home_intent = new Intent(getApplicationContext(), SettingsActivity.class)
+                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(home_intent);
+    }
+}
