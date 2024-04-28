38807bc9acdb420e90d938e3485a2a5cec0916ba
Daniel Pham <15070397+exKitsune@users.noreply.github.com>
2024-04-28 04:19:36 -0400

    End to end encryption on server

--- Server/src/bin/client.rs
+++ Server/src/bin/client.rs
@@ -1,9 +1,24 @@
+use std::convert::TryInto;
 use std::io;
 
+use noise_protocol::DH;
+use noise_rust_crypto::X25519;
+use server::{encrypt_client_message, Pubkey};
+
 fn main() -> io::Result<()> {
+    let server_key = base64::decode(std::env::args().skip(1).next().unwrap()).unwrap();
+    let server_key = server_key[..32].try_into().unwrap();
+    let client_static_key = X25519::genkey();
+    println!("{:?}", X25519::pubkey(&client_static_key));
     let client = reqwest::blocking::Client::new();
+    let message = encrypt_client_message(
+        "https://www.youtube.com/watch?v=mZ0sJQC8qkE",
+        client_static_key,
+        server_key,
+    )
+    .unwrap();
     let addr = std::env::args()
-        .skip(1)
+        .skip(2)
         .next()
         .unwrap_or("127.0.0.1:8998".into());
     let resp = client
@@ -12,7 +27,7 @@ fn main() -> io::Result<()> {
                 .parse::<reqwest::Url>()
                 .unwrap(),
         )
-        .body("https://www.youtube.com/watch?v=mZ0sJQC8qkE")
+        .body(message)
         .send()
         .unwrap();
     Ok(())
