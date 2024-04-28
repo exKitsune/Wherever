5a351b54d6889b3350850df7b384e3afb5c92ccd
Daniel Pham <15070397+exKitsune@users.noreply.github.com>
2024-04-28 04:20:04 -0400

    Made app show public key

--- crypto_java/WhereverCrypto.java
+++ crypto_java/WhereverCrypto.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 class WhereverCrypto {
     private static native byte[] encryptMessage(String input, byte[] client_key, byte[] serverKey, long sequence);
     private static native byte[] generateKey();
+    private static native byte[] getPubkey(byte[] client_key);
 
     static {
         System.loadLibrary("wherever_crypto_java");
