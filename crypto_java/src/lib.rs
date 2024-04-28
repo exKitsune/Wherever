15fbfbb43cb94d0bd703ebd83c92b4fb089e3550
Daniel Pham <15070397+exKitsune@users.noreply.github.com>
2024-04-28 04:20:00 -0400

    Add java wrapper for crypto. Rename Server to server

---
+++ crypto_java/src/lib.rs
@@ -0,0 +1,52 @@
+use std::convert::TryInto;
+
+use jni::objects::{JClass, JString, JValue};
+use jni::sys::jbyteArray;
+
+use jni::JNIEnv;
+
+use wherever_crypto::{Key, Pubkey};
+use wherever_crypto::{U8Array, DH, X25519};
+
+#[no_mangle]
+pub extern "system" fn Java_WhereverCrypto_encryptMessage(
+    env: JNIEnv,
+    _class: JClass,
+    input: JString,
+    client_key: jbyteArray,
+    server_key: jbyteArray,
+) -> jbyteArray {
+    let input: String = env
+        .get_string(input)
+        .expect("Couldn't get java string")
+        .into();
+
+    let client_key = env.convert_byte_array(client_key).unwrap();
+    let client_key = Key::from_slice(&client_key);
+    let server_key = env
+        .convert_byte_array(server_key)
+        .unwrap()
+        .try_into()
+        .unwrap();
+
+    let msg = wherever_crypto::encrypt_client_message(&input, client_key, server_key).unwrap();
+    let output = env.byte_array_from_slice(&msg).unwrap();
+
+    output
+}
+
+#[no_mangle]
+pub extern "system" fn Java_WhereverCrypto_generateKey(env: JNIEnv, _class: JClass) -> jbyteArray {
+    let key = X25519::genkey();
+    println!("ur mom ");
+    println!("{:?}", &*key);
+    env.byte_array_from_slice(&*key).unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
