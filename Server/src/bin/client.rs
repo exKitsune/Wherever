cb6b10a0ca1b7b54f2464151e483433f5180ebd8
Daniel Pham <15070397+exKitsune@users.noreply.github.com>
2024-04-28 04:19:36 -0400

    Relay server & client initial implementation?

--- Server/src/bin/client.rs
+++ Server/src/bin/client.rs
@@ -2,8 +2,16 @@ use std::io;
 
 fn main() -> io::Result<()> {
     let client = reqwest::blocking::Client::new();
+    let addr = std::env::args()
+        .skip(1)
+        .next()
+        .unwrap_or("127.0.0.1:8998".into());
     let resp = client
-        .post("http://127.0.0.1:8998/open")
+        .post(
+            format!("http://{}/open", addr)
+                .parse::<reqwest::Url>()
+                .unwrap(),
+        )
         .body("https://www.youtube.com/watch?v=mZ0sJQC8qkE")
         .send()
         .unwrap();
--- Server/src/main.rs
+++ Server/src/main.rs
@@ -1,34 +1,155 @@
-use std::io::{self};
+use std::collections::HashMap;
 use std::net::SocketAddr;
+use std::sync::Arc;
 use std::thread;
 
-use tiny_http::{Method, Response};
+use warp::ws::{self, WebSocket};
+use warp::Filter;
 
-fn main() -> io::Result<()> {
+use futures::{select, FutureExt, SinkExt, StreamExt};
+use tokio::sync::{mpsc, RwLock};
+
+#[tokio::main]
+async fn main() {
     let addr: SocketAddr = ([0, 0, 0, 0], 8998).into();
-    qr2term::print_qr(format!("where://{}", addr));
-    let succ_response = || Response::new(200.into(), vec![], b"Yeet".as_ref(), Some(4), None);
-    let fail_response = || Response::new(400.into(), vec![], b"Not Good".as_ref(), Some(8), None);
-    let server =
-        tiny_http::Server::http(addr).map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
-    server
-        .incoming_requests()
-        .inspect(|req| {
-            println!("{:?}", req);
-        })
-        .map(|mut req| match (req.method(), req.url()) {
-            (Method::Post, "/open") => {
-                let mut url = String::new();
-                req.as_reader().read_to_string(&mut url)?;
-                println!("Opening {:?}", url);
-                thread::spawn(|| open::that_in_background(url));
-                req.respond(succ_response())
+    let mut args = std::env::args().skip(1);
+    match args.next().as_deref() {
+        None | Some("standalone") => {
+            let addr = args
+                .next()
+                .map(|s| s.parse())
+                .unwrap_or(Ok(addr))
+                .expect("Invalid host");
+            qr2term::print_qr(format!("where://{}", addr)).unwrap();
+            standalone(addr).await;
+        }
+        Some("relay_server") => {
+            let addr = args
+                .next()
+                .map(|s| s.parse())
+                .unwrap_or(Ok(addr))
+                .expect("Invalid host");
+            println!("Starting relay server");
+            qr2term::print_qr(format!("where://{}", addr)).unwrap();
+            relay(addr).await;
+        }
+        Some("relay_client") => {
+            println!("Starting relay client");
+            qr2term::print_qr(format!("where://{}", addr)).unwrap();
+            if let Some(addr) = args.next().and_then(|s| s.parse().ok()) {
+                relay_client(addr);
+            } else {
+                eprintln!("Relay server address required");
             }
-            _ => req.respond(fail_response()),
-        })
-        .for_each(|res| {
-            println!("{:?}", res);
-        });
+        }
+        Some(arg) => {
+            eprintln!("Unrecognized argument: \"{}\"", arg);
+        }
+    }
+}
 
-    Ok(())
+async fn standalone(addr: SocketAddr) {
+    warp::serve(
+        warp::path("open")
+            .and(warp::post())
+            .and(warp::body::content_length_limit(4096))
+            .and(warp::body::bytes())
+            .and_then(|body: warp::hyper::body::Bytes| async move {
+                let url = std::str::from_utf8(&body).map_err(|_| warp::reject::reject())?;
+                launch(url.to_owned());
+                Ok::<_, warp::Rejection>("Good")
+            }),
+    )
+    .run(addr)
+    .await;
+}
+
+fn launch(url: String) {
+    println!("Opening {:?}", url);
+    thread::spawn(|| open::that_in_background(url));
+}
+
+#[derive(Eq, PartialEq, Hash, Clone)]
+struct Key(u64); // TODO:
+
+struct Message(String);
+
+type Registry = HashMap<Key, mpsc::Sender<Message>>;
+
+async fn relay(addr: SocketAddr) {
+    let registry = Arc::new(RwLock::new(Registry::new()));
+    let routes = warp::path("stream")
+        .and(warp::ws())
+        .and(warp::filters::addr::remote())
+        .map({
+            let registry = registry.clone();
+            move |ws: warp::ws::Ws, addr| {
+                let registry = registry.clone();
+                ws.on_upgrade(move |websocket| handle_client(websocket, registry, addr))
+            }
+        })
+        .or(warp::path("open")
+            .and(warp::post())
+            .and(warp::body::content_length_limit(4096))
+            .and(warp::body::bytes())
+            .and_then(move |body: warp::hyper::body::Bytes| {
+                let registry = registry.clone();
+                async move {
+                    let body = std::str::from_utf8(&body).map_err(|_| warp::reject::reject())?;
+                    println!("AAAA {}", body);
+                    for (_key, channel) in registry.read().await.iter() {
+                        channel.send(Message(body.to_owned())).await.ok().unwrap();
+                    }
+                    Ok::<_, warp::Rejection>("Good")
+                }
+            }));
+    warp::serve(routes).run(addr).await;
+}
+
+async fn handle_client(
+    mut ws: WebSocket,
+    state: Arc<RwLock<Registry>>,
+    remote: Option<SocketAddr>,
+) {
+    println!("Client connected {:?}", remote);
+    // get public key of client
+    let key = Key(1234); // TODO:
+                         // register key + channel in shared state
+    let (sender, mut receiver) = mpsc::channel(1);
+    state.write().await.insert(key.clone(), sender);
+    {
+        let (mut outgoing, mut incoming) = (&mut ws).split();
+        let out_ref = &mut outgoing;
+        // wait for messages
+        while let Ok(()) = select! {
+            res = receiver.recv().then(|msg| async {
+                // process message, send to
+                if let Some(msg) = msg {
+                    out_ref.send(ws::Message::text(msg.0)).await.map_err(|_|())
+                } else {
+                    Err(())
+                }
+            }) => res,
+            res = incoming.next().map(|msg| match msg {
+                Some(Ok(msg)) if msg.is_close() => Err(()),
+                Some(Ok(_)) => Ok(()),
+                Some(Err(_)) => Err(()),
+                None => Err(()),
+            }) => res
+        } {}
+    }
+    // on close unregister key
+    state.write().await.remove(&key);
+    println!("Client disconnected {:?}", remote);
+    let _ = ws.close().await;
+}
+
+fn relay_client(addr: SocketAddr) {
+    let (mut socket, _resp) =
+        tungstenite::client::connect(format!("ws://{}/stream", addr)).unwrap();
+    while let Ok(msg) = socket.read_message() {
+        if let Ok(url) = msg.into_text() {
+            launch(url)
+        }
+    }
 }
